771c292862f19f6541550975b613f5050cd4aa8a
M-jerez <jerez.m82@gmail.com>
2018-08-12 22:57:52 +0100

    remove unused package

--- packages/apids-json-schema/__tests__/apids-json-schema.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const apidsJsonSchema = require('..');
-
-describe('apids-json-schema', () => {
-    it('needs tests');
-});
--- packages/apids-json-schema/lib/apids-json-schema.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = apidsJsonSchema;
-
-function apidsJsonSchema() {
-    // TODO
-}
