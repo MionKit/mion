76e294817c45d0d1e6c035302dc42db21c48fef1
Ma jerez <jerez.m82@gmail.com>
2022-10-09 15:58:51 +0200

    remove non required files and update docs

--- packages/router/test-artifacts/typescript-ast/fail-route-options-with-no-type.ts
+++
@@ -1,6 +0,0 @@
-// route options no types
-export const optionsObjectWithNoTypes = {
-    handler: (body) => ({sentence: `hello to ${body.username}`}),
-    version: '1.0.0',
-    logLevel: 'debug',
-};
