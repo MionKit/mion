c6d6b605678b9958731972f23ea15b2be14a5d9a
Ma jerez <jerez.m82@gmail.com>
2022-10-09 20:21:29 +0200

    clean code

--- packages/router/src/constants.ts
+++ packages/router/src/constants.ts
@@ -4,9 +4,3 @@
  * License: MIT
  * The software is provided "as is", without warranty of any kind.
  * ######## */
-
-export const API_ROUTE_PARAMS_LENGTH = 4;
-export const ALLOWED_EXTENSIONS = ['.ts', '.tsx', '.mts'];
-export const ABSOLUTE_PATH_TOKEN = '/{{PROJECT_ROOT}}'; // used to sanitise absolute path in metadata output
-// the types of the parameters of the router function call, keep same order as in the ApiRoute
-export const ROUTE_PARAMETERS_EXPECTED_TYPE_NAME = ['any', 'FastifyRequest', 'FastifyReply', 'FastifyInstance'];
--- packages/router/src/router.ts
+++ packages/router/src/router.ts
@@ -4,13 +4,3 @@
  * License: MIT
  * The software is provided "as is", without warranty of any kind.
  * ######## */
-
-import {ApiRouterConfig, ApiRoute, ApiRouteOptions} from './types';
-
-function addApiRoute<Req, Resp>(path: string, route: ApiRoute<Req, Resp>) {}
-
-function addApiRouteOptions<Req, Resp>(path: string, route: ApiRouteOptions<Req, Resp>) {}
-
-export function addApiRoutes<Req, Resp>(path: string, routes: (ApiRoute<Req, Resp> | ApiRouteOptions<Req, Resp>)[]) {}
-
-function validateApiModue() {}
