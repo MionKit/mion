01ae0be4525f3738d6376b497608c8d9bbe21c8f
Ma jerez <jerez.m82@gmail.com>
2022-10-20 20:59:12 +0200

    move @deepkit dependencies to the correct place

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,5 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   coverageDirectory: '.dist',
-  collectCoverageFrom: ['packages/**/src/**'],
+  collectCoverageFrom: ['packages/*/src/**'],
 };
