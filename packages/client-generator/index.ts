aaece4682a94c9e96579c1d011b2d77210d539ce
Ma jerez <jerez.m82@gmail.com>
2023-01-11 16:12:39 +0100

    delete client generator package 😢

--- packages/client-generator/index.ts
+++
@@ -1,8 +0,0 @@
-/* ########
- * 2022 MikroKit
- * Author: Ma-jerez
- * License: MIT
- * The software is provided "as is", without warranty of any kind.
- * ######## */
-
-export * from './src/clientGenerator';
