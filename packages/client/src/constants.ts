23924221a3105b0ed29c90d5a0aee715bcc7c858
Ma jerez <jerez.m82@gmail.com>
2023-07-26 22:20:01 +0100

    update typescript configuration so client uses window.fetch instead node-fetch

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -7,16 +7,6 @@
 
 import {ClientOptions} from './types';
 
-// TODO: user @types/web, instead bellow, but there are conflicts with @types/node
-// we probably need to move @types/node out of the root and add to each individual package
-declare global {
-    interface Window {
-        location: any;
-    }
-
-    const window: Window;
-}
-
 export const DEFAULT_PREFILL_OPTIONS: ClientOptions = {
     apiURL: window.location.origin,
     prefillStorage: 'localStorage',
