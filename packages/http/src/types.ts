f969045f2d238100b3ea656d20918c06b3eb9da0
Ma jerez <jerez.m82@gmail.com>
2023-08-06 22:58:51 +0100

    update packages to fix tests

--- packages/http/src/types.ts
+++ packages/http/src/types.ts
@@ -5,7 +5,6 @@
  * The software is provided "as is", without warranty of any kind.
  * ######## */
 
-import * as undici_types from 'undici';
 import {Headers, RouterOptions} from '@mionkit/router';
 import {IncomingMessage, ServerResponse} from 'http';
 import {ServerOptions} from 'https';
@@ -34,24 +33,24 @@ export type HttpOptions = {
 
 export type HttpRequest = IncomingMessage & {body: string};
 
-// fix for missing fetch types in node 18
-// @see https://stackoverflow.com/questions/71294230/how-can-i-use-native-fetch-with-node-in-typescript-node-v17-6
-declare global {
-    export const {fetch, FormData, Headers, Request, Response}: typeof import('undici');
+// // fix for missing fetch types in node 18
+// // @see https://stackoverflow.com/questions/71294230/how-can-i-use-native-fetch-with-node-in-typescript-node-v17-6
+// declare global {
+//     export const {fetch, FormData, Headers, Request, Response}: typeof import('undici');
 
-    type FormData = undici_types.FormData;
-    type Headers = undici_types.Headers;
-    type HeadersInit = undici_types.HeadersInit;
-    type BodyInit = undici_types.BodyInit;
-    type Request = undici_types.Request;
-    type RequestInit = undici_types.RequestInit;
-    type RequestInfo = undici_types.RequestInfo;
-    type RequestMode = undici_types.RequestMode;
-    type RequestRedirect = undici_types.RequestRedirect;
-    type RequestCredentials = undici_types.RequestCredentials;
-    type RequestDestination = undici_types.RequestDestination;
-    type ReferrerPolicy = undici_types.ReferrerPolicy;
-    type Response = undici_types.Response;
-    type ResponseInit = undici_types.ResponseInit;
-    type ResponseType = undici_types.ResponseType;
-}
+//     type FormData = undici_types.FormData;
+//     type Headers = undici_types.Headers;
+//     type HeadersInit = undici_types.HeadersInit;
+//     type BodyInit = undici_types.BodyInit;
+//     type Request = undici_types.Request;
+//     type RequestInit = undici_types.RequestInit;
+//     type RequestInfo = undici_types.RequestInfo;
+//     type RequestMode = undici_types.RequestMode;
+//     type RequestRedirect = undici_types.RequestRedirect;
+//     type RequestCredentials = undici_types.RequestCredentials;
+//     type RequestDestination = undici_types.RequestDestination;
+//     type ReferrerPolicy = undici_types.ReferrerPolicy;
+//     type Response = undici_types.Response;
+//     type ResponseInit = undici_types.ResponseInit;
+//     type ResponseType = undici_types.ResponseType;
+// }
