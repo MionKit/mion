411a133682605901b7a12c0104017b2cf510d1e9
Ma jerez <jerez.m82@gmail.com>
2023-08-13 16:30:07 +0100

    Mion Client ready with methods to call validate and persist data

--- packages/client/src/client.spec.ts
+++ packages/client/src/client.spec.ts
@@ -7,9 +7,16 @@
 
 import {RemoteMethods, Routes, registerRoutes} from '@mionkit/router';
 import {initMionClient} from './client';
-import {HookRequest, RouteRequest} from './types';
+import {HookSubRequest, RouteSubRequest} from './types';
 import {initHttpRouter, startHttpServer} from '@mionkit/http';
+import {clientRoutes} from '@mionkit/common';
 import {Server} from 'http';
+import {PublicError} from '@mionkit/core';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Storage = require('dom-storage');
+global.localStorage = new Storage(null, {strict: true});
+global.sessionStorage = new Storage(null, {strict: true});
 
 // TODO: test & write client
 describe('client', () => {
@@ -21,11 +28,13 @@ describe('client', () => {
             canReturnData: true,
             headerHook: (ctx, token: string): User => ({name: 'John', surname: 'Doe'}),
         },
-        sayHello: {route: (ctx, user: User): string => `Hello ${user.name} ${user.surname}`},
+        sayHello: {route: (ctx, user: User): string | PublicError => `Hello ${user.name} ${user.surname}`},
+        alwaysFails: (ctx, user: User): User | PublicError =>
+            new PublicError({statusCode: 500, message: 'Something fails', name: 'UnknownError'}),
         utils: {
             sumTwo: (ctx, a: number): number => a + 2,
         },
-        log: {hook: (ctx): void => console.log('ending')},
+        log: {hook: (ctx): any => 'Logger.log(....)'},
     } satisfies Routes;
 
     const someUser = {name: 'John', surname: 'Doe'};
@@ -37,6 +46,7 @@ describe('client', () => {
     beforeAll(async () => {
         initHttpRouter({sharedDataFactory: () => {}, port});
         myApi = registerRoutes(routes);
+        registerRoutes(clientRoutes);
         server = await startHttpServer();
     });
 
@@ -51,9 +61,9 @@ describe('client', () => {
     );
 
     it('proxy to trap remote methods calls and return MethodRequest data', () => {
-        const {client, methods} = initMionClient<MyApi>({baseURL: 'http://localhost:3000'});
+        const {client, methods} = initMionClient<MyApi>({baseURL: `http://localhost:${port}`});
 
-        const expectedAuth: HookRequest<any> = {
+        const expectedAuthSubRequest = {
             pointer: ['auth'],
             id: 'auth',
             isResolved: false,
@@ -61,7 +71,7 @@ describe('client', () => {
             persist: expect.any(Function),
         };
 
-        const expectedSayHello: RouteRequest<any> = {
+        const expectedSayHelloSubRequest = {
             pointer: ['sayHello'],
             id: 'sayHello',
             isResolved: false,
@@ -69,7 +79,7 @@ describe('client', () => {
             call: expect.any(Function),
         };
 
-        const expectedSumTwo: HookRequest<any> = {
+        const expectedSumTwoSubRequest = {
             pointer: ['utils', 'sumTwo'],
             id: 'utils-sumTwo',
             isResolved: false,
@@ -77,32 +87,72 @@ describe('client', () => {
             persist: expect.any(Function),
         };
 
-        expect(methods.auth('token')).toEqual(expect.objectContaining(expectedAuth));
-        expect(methods.sayHello(someUser)).toEqual(expect.objectContaining(expectedSayHello));
-        expect(methods.utils.sumTwo(2)).toEqual(expect.objectContaining(expectedSumTwo));
+        expect(methods.auth('token')).toEqual(expect.objectContaining(expectedAuthSubRequest));
+        expect(methods.sayHello(someUser)).toEqual(expect.objectContaining(expectedSayHelloSubRequest));
+        expect(methods.utils.sumTwo(2)).toEqual(expect.objectContaining(expectedSumTwoSubRequest));
 
         // is a proxy so actually could trap any call even if does not exists in methods and is not strongly typed
         // note bellow code should not be used when using the client
-        const expectedUntyped: RouteRequest<any> & HookRequest<any> = {
+        const expectedUnknownSubRequest: RouteSubRequest<any> & HookSubRequest<any> = {
             pointer: ['abcd'],
             id: 'abcd',
             isResolved: false,
             params: [1, 'a'],
             call: expect.any(Function),
             persist: expect.any(Function),
+            validate: expect.any(Function),
         };
-        expect((methods as any).abcd(1, 'a')).toEqual(expectedUntyped);
+        expect((methods as any).abcd(1, 'a')).toEqual(expectedUnknownSubRequest);
     });
 
     // TODO: jest upgrade required to include nadive node fetch types without using jsdom
-    it.skip('fetch a route', async () => {
+    it('make a route call and get a valid response', async () => {
         // TODO: implement the get public method info route in the router
-        const {client, methods} = initMionClient<MyApi>({baseURL: 'http://localhost:3000'});
+        const {methods} = initMionClient<MyApi>({baseURL: `http://localhost:${port}`});
 
         const response = await methods.sayHello(someUser).call(methods.auth('token'));
-
         expect(response).toEqual(`Hello John Doe`);
     });
 
-    it('fail if a remote call fails', () => {});
+    it('throw error if a route call fails', async () => {
+        // TODO: implement the get public method info route in the router
+        const {client, methods} = initMionClient<MyApi>({baseURL: `http://localhost:${port}`});
+
+        let error;
+        const expectedError = new PublicError({
+            message: 'Something fails',
+            name: 'UnknownError',
+            statusCode: 500,
+        });
+
+        try {
+            await methods.alwaysFails(someUser).call(methods.auth('token'));
+        } catch (e) {
+            error = e;
+        }
+
+        expect(error).toEqual(expectedError);
+        expect(error.statusCode).toEqual(expectedError.statusCode);
+    });
+
+    it('throw error if a route is missing hook data', async () => {
+        // TODO: implement the get public method info route in the router
+        const {client, methods} = initMionClient<MyApi>({baseURL: `http://localhost:${port}`});
+
+        let error;
+        const expectedError = new PublicError({
+            message: `Invalid params for Route or Hook 'auth', validation failed.`,
+            name: 'Validation Error',
+            statusCode: 400,
+        });
+
+        try {
+            await methods.sayHello(someUser).call();
+        } catch (e) {
+            error = e;
+        }
+
+        expect(error).toEqual(expectedError);
+        expect(error.statusCode).toEqual(expectedError.statusCode);
+    });
 });
