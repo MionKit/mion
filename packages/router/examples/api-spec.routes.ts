3f3dbb40e737c779a39433d00c6d7e75de72a0a8
Ma jerez <jerez.m82@gmail.com>
2023-08-14 00:29:17 +0100

    add client documentation

--- packages/router/examples/api-spec.routes.ts
+++ packages/router/examples/api-spec.routes.ts
@@ -25,22 +25,4 @@ const hooks = {
     log: {hook: (c: Context): void => undefined},
 } satisfies Routes;
 
-const routesSpec = registerRoutes(routes);
-type RoutesSpec = typeof routesSpec;
-type SayHello = RoutesSpec['sayHello']; // type should be PublicRoute
-type SayHello2 = RoutesSpec['sayHello2']; // type should be PublicRoute
-type SayHello3 = RoutesSpec['sayHello3']; // type should be PublicRoute
-type SayHello4 = RoutesSpec['sayHello4']; // type should be PublicRoute
-type SayHelloReturn = ReturnType<RoutesSpec['sayHello']['_handler']>; // should be [string]
-type SayHelloErrorReturn = ReturnType<RoutesSpec['sayHelloError']['_handler']>; // should be [null, PublicError]
-type maybeErrorReturn = ReturnType<RoutesSpec['maybeError']['_handler']>; // should be [string | null, PublicError]
-
-const hooksSpec = registerRoutes(hooks);
-type HookSpec = typeof hooksSpec;
-type Auth = HookSpec['auth']; // type should be PublicHook as has parameters
-type Parser = HookSpec['parser']; // type should be null
-type Parser2 = HookSpec['parser2']; // type should be null
-type HookNoCtx = HookSpec['hookNoCtx']; // type should be null
-type HookParams = HookSpec['hookParams']; // type should be PublicHook as has params
-type HookCanReturn = HookSpec['hookCanReturn']; // type should be PublicHook as canReturnData
-type Log = HookSpec['log']; // type should be null
+export const routesSpec = registerRoutes(routes);
