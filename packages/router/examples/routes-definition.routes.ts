304108f86792e3930aa9ad5bc0952bfd99edb73b
Ma jerez <jerez.m82@gmail.com>
2023-08-21 02:03:52 +0100

    Continue  with documentation site, use embedme for source code, add mermaid pluggin

--- packages/router/examples/routes-definition.routes.ts
+++ packages/router/examples/routes-definition.routes.ts
@@ -1,19 +1,20 @@
-import {registerRoutes, Routes, initRouter} from '@mionkit/router';
+import { RouteDef, Routes } from '@mionkit/router';
 
+// Defining a route as Handler
 const sayHello = (ctx, name: string): string => {
     return `Hello ${name}.`;
 };
 
+// Using a RouteDef object
 const sayHello2 = {
+    enableSerialization: false,
+    enableValidation: false,
     route(ctx, name1: string, name2: string): string {
         return `Hello ${name1} and ${name2}.`;
     },
-};
+} satisfies RouteDef;
 
 const routes = {
-    sayHello, // api/sayHello
-    sayHello2, // api/sayHello2
+    sayHello,
+    sayHello2,
 } satisfies Routes;
-
-initRouter({prefix: 'api/'});
-export const apiSpec = registerRoutes(routes);
--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -6,8 +6,6 @@ export default defineNuxtConfig({
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible',
     // https://github.com/nuxt/devtools
-    '@nuxt/devtools',
-    // https://github.com/phojie/nuxt-content-snippet
-    'nuxt-content-snippet'
+    '@nuxt/devtools'
   ]
 })
