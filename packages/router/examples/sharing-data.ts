b7201950218cbf24c58e783f089af679ae1a82f1
Ma jerez <jerez.m82@gmail.com>
2023-09-04 23:50:40 +0100

    fix  style on router examples

--- packages/router/examples/sharing-data.ts
+++ packages/router/examples/sharing-data.ts
@@ -1,13 +1,13 @@
-import { RpcError } from '@mionkit/core';
-import { HeaderHookDef, Routes, registerRoutes } from '@mionkit/router';
-import { getAuthUser, isAuthorized } from 'MyAuth';
+import {RpcError} from '@mionkit/core';
+import {HeaderHookDef, Routes, registerRoutes} from '@mionkit/router';
+import {getAuthUser, isAuthorized} from 'MyAuth';
 
 const authorizationHook = {
     headerName: 'authorization',
     async headerHook(context, token: string): Promise<void> {
         const me = await getAuthUser(token);
         if (!isAuthorized(me)) {
-            throw new RpcError({ statusCode: 401, publicMessage: 'user is not authorized' });
+            throw new RpcError({statusCode: 401, publicMessage: 'user is not authorized'});
         }
         context.shared.myUser = me; // user is added to ctx to shared with other routes/hooks
     },
