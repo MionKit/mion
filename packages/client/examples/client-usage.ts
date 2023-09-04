a545214eec04df8e7c7916e254dd0d6b7ac5e7cb
Ma jerez <jerez.m82@gmail.com>
2023-09-04 23:14:40 +0100

    client documentation.  Documentation finished and ready to go!

--- packages/client/examples/client-usage.ts
+++ packages/client/examples/client-usage.ts
@@ -8,16 +8,16 @@ const port = 8076;
 const baseURL = `http://localhost:${port}`;
 const {methods, client} = initClient<MyApi>({baseURL});
 
+// calls sumTwo route in the server
+const authSubRequest = methods.auth('myToken-XYZ');
+const sumTwoResp = await methods.utils.sum(5, 2).call(authSubRequest);
+console.log(sumTwoResp); // 7
+
 // prefills the token for any future requests, value is stored in localStorage
 await methods.auth('myToken-XYZ').prefill();
-
-// calls sayHello route in the server
-const sayHello = await methods.utils.sayHello({id: '123', name: 'John', surname: 'Doe'}).call();
-console.log(sayHello); // Hello John Doe
-
-// calls sumTwo route in the server
-const sumTwoResp = await methods.utils.sum(5, 2).call(methods.auth('myToken-XYZ'));
-console.log(sumTwoResp); // 7
+// // calls sumTwo route in the server
+const sumTwoResponse = await methods.utils.sum(5, 2).call();
+console.log(sumTwoResponse); // 7
 
 // validate parameters locally without calling the server
 const validationResp: ParamsValidationResponse = await methods.utils
