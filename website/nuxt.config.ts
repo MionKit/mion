ed34f4b2f69846e091d68e2065e5e55d889790e3
Ma jerez <jerez.m82@gmail.com>
2023-09-05 03:41:19 +0100

    website tweaks

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -2,6 +2,8 @@ export default defineNuxtConfig({
     // https://github.com/nuxt-themes/docus
     extends: '@nuxt-themes/docus',
 
+    css: ['~/assets/css/mion.css'],
+
     app: {
         baseURL: '/mion/', // working with github pages mionkit.github.io/mion/ - Remove when moving to mion.io
         buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
