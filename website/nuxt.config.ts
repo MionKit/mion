7aee8cc62d69d2fe3c65798c1766f8f94517e3a7
Ma jerez <jerez.m82@gmail.com>
2023-09-05 02:08:19 +0100

    set nuxt base pat to work with github pages

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -1,11 +1,16 @@
 export default defineNuxtConfig({
-  // https://github.com/nuxt-themes/docus
-  extends: '@nuxt-themes/docus',
+    // https://github.com/nuxt-themes/docus
+    extends: '@nuxt-themes/docus',
 
-  modules: [
-    // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible',
-    // https://github.com/nuxt/devtools
-    '@nuxt/devtools'
-  ]
-})
+    app: {
+        baseURL: '/mion/', // working with github pages mionkit.github.io/mion/ - Remove when moving to mion.io
+        buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+    },
+
+    modules: [
+        // https://github.com/nuxt-modules/plausible
+        '@nuxtjs/plausible',
+        // https://github.com/nuxt/devtools
+        '@nuxt/devtools',
+    ],
+});
