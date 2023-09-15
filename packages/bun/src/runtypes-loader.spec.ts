4031f4d1b3243177f8a1727e9cdf0d8a82dc5fdc
Ma jerez <jerez.m82@gmail.com>
2023-09-15 19:01:28 +0100

    create bun package

---
+++ packages/bun/src/runtypes-loader.spec.ts
@@ -0,0 +1,10 @@
+/* ########
+ * 2023 mion
+ * Author: Ma-jerez
+ * License: MIT
+ * The software is provided "as is", without warranty of any kind.
+ * ######## */
+
+describe('bun runtime loader should', () => {
+    it('do something cool', async () => {});
+});
