54f8062b11a483d90bed252895e1bebd89066bcf
Ma jerez <jerez.m82@gmail.com>
2023-09-15 19:01:28 +0100

    create bun package

---
+++ packages/bun/README.md
@@ -0,0 +1,27 @@
+<p align="center">
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/MionKit/mion/master/assets/public/bannerx90-dark.png">
+    <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/MionKit/mion/master/assets/public/bannerx90.png">
+    <img alt='mion, a mikro kit for Typescript Serverless APIs' src='https://raw.githubusercontent.com/MionKit/mion/master/assets/public/bannerx90.png'>
+  </picture>
+</p>
+<p align="center">
+  <strong>mion http server for bun.
+  </strong>
+</p>
+<p align=center>
+  <img src="https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square&maxAge=99999999" alt="npm"  style="max-width:100%;">
+  <img src="https://img.shields.io/badge/license-MIT-97ca00.svg?style=flat-square&maxAge=99999999" alt="npm"  style="max-width:100%;">
+</p>
+
+# `@mionkit/bun`
+
+mion http server for bun.
+
+## Check Out The [Website And Documentation](http://mion.io) 📚
+
+[![mion-website-banner](https://raw.githubusercontent.com/MionKit/mion/master/assets/public/mion-website-banner.png)](http://mion.io)
+
+---
+
+[MIT LICENSE](../../LICENSE)
---
+++ packages/bun/src/runtypes-loader.ts
@@ -0,0 +1,105 @@
+/* ########
+ * 2023 mion
+ * Author: Ma-jerez
+ * License: MIT
+ * The software is provided "as is", without warranty of any kind.
+ * ######## */
+
+import {GET_REMOTE_METHODS_BY_ID, GET_REMOTE_METHODS_BY_PATH, AnyObject, RpcError} from '@mionkit/core';
+import {
+    RemoteMethodMetadata,
+    getHookExecutable,
+    getRouteExecutable,
+    isPrivateExecutable,
+    getMethodMetadataFromExecutable,
+    getRouteExecutionPath,
+    Routes,
+    getRouterOptions,
+    RouterOptions,
+    getTotalExecutables,
+    getAllExecutablesIds,
+    getAnyExecutable,
+    Executable,
+} from '@mionkit/router';
+
+export type RemoteMethodsDictionary = {[key: string]: RemoteMethodMetadata};
+export interface ClientRouteOptions extends RouterOptions {
+    getAllRemoteMethodsMaxNumber?: number;
+}
+
+export const defaultClientRouteOptions = {
+    getAllRemoteMethodsMaxNumber: 100,
+};
+
+function addRequiredRemoteMethodsToResponse(id: string, resp: RemoteMethodsDictionary, errorData: AnyObject): void {
+    if (resp[id]) return;
+    const executable = getHookExecutable(id) || getRouteExecutable(id);
+    if (!executable) {
+        errorData[id] = `Remote Method ${id} not found`;
+        return;
+    }
+    if (isPrivateExecutable(executable)) return;
+
+    resp[id] = getMethodMetadataFromExecutable(executable);
+    resp[id].hookIds?.forEach((hookId) => addRequiredRemoteMethodsToResponse(hookId, resp, errorData));
+}
+
+export const getRemoteMethods = (
+    ctx,
+    methodsIds: string[],
+    getAllRemoteMethods?: boolean
+): RemoteMethodsDictionary | RpcError => {
+    const resp: RemoteMethodsDictionary = {};
+    const errorData = {};
+    const maxMethods =
+        getRouterOptions<ClientRouteOptions>().getAllRemoteMethodsMaxNumber ||
+        defaultClientRouteOptions.getAllRemoteMethodsMaxNumber;
+    const shouldReturnAll = getAllRemoteMethods && getTotalExecutables() <= maxMethods;
+    const idsToReturn = shouldReturnAll
+        ? getAllExecutablesIds().filter(
+              (id) =>
+                  id !== GET_REMOTE_METHODS_BY_ID &&
+                  id !== GET_REMOTE_METHODS_BY_PATH &&
+                  !isPrivateExecutable(getAnyExecutable(id) as Executable)
+          )
+        : methodsIds;
+    idsToReturn.forEach((id) => addRequiredRemoteMethodsToResponse(id, resp, errorData));
+
+    if (Object.keys(errorData).length)
+        return new RpcError({
+            statusCode: 404,
+            name: 'Invalid Metadata Request',
+            publicMessage: 'Errors getting Remote Methods Metadata',
+            errorData,
+        });
+    return resp;
+};
+
+export const getRouteRemoteMethods = (ctx, path: string, getAllRemoteMethods?: boolean): RemoteMethodsDictionary | RpcError => {
+    const executables = getRouteExecutionPath(path);
+    if (!executables)
+        return new RpcError({
+            statusCode: 404,
+            name: 'Invalid Metadata Request',
+            publicMessage: `Route ${path} not found`,
+        });
+    const privateExecutables = executables.filter((e) => !isPrivateExecutable(e));
+    return getRemoteMethods(
+        ctx,
+        privateExecutables.map((e) => e.id),
+        getAllRemoteMethods
+    );
+};
+
+export const clientRoutes = {
+    [GET_REMOTE_METHODS_BY_ID]: {
+        // disable serialization as deserializer seems ti ignore serializedTypes
+        enableSerialization: false,
+        route: getRemoteMethods,
+    },
+    [GET_REMOTE_METHODS_BY_PATH]: {
+        enableSerialization: false,
+        // disable serialization as deserializer seems ti ignore serializedTypes
+        route: getRouteRemoteMethods,
+    },
+} satisfies Routes;
