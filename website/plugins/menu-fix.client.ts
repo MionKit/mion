909b5decf36ab5e6c74dda8899477b220dad0056
Ma jerez <jerez.m82@gmail.com>
2023-10-15 15:48:44 +0100

    website: fix mobile navigation and hide toc in mobile

---
+++ website/plugins/menu-fix.client.ts
@@ -0,0 +1,11 @@
+export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.hook('page:finish', () => {        
+        setTimeout(() => {
+            // commented code is not working so we need to use plain js
+            // const menu: any = useMenu();
+            // menu.close();
+            const closeBtn: HTMLAnchorElement | null = document.querySelector("nav.dialog .wrapper button[aria-label='Menu']");
+            if (closeBtn) closeBtn.click();
+        }, 100)
+    })
+  })
