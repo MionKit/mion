8eb7d561a18ee2da1e61725dd5c61ea9cdd60d61
Ma jerez <jerez.m82@gmail.com>
2023-10-15 02:18:33 +0100

    add typed effect to the title and update homepage

---
+++ website/components/content/PlatformTiles.vue
@@ -0,0 +1,40 @@
+<script setup>
+</script>
+<template>
+  <div class="platform-tiles">
+    <div class="tile">
+      <nuxt-img src="/platforms/bun-logo.svg" class="platform-img" loading="lazy" />
+    </div>
+    <div class="tile">
+      <nuxt-img src="/platforms/node-logo.svg" class="platform-img" loading="lazy" />
+    </div>
+    <div class="tile">
+      <nuxt-img src="/platforms/aws-lambda-logo.svg" class="platform-img" loading="lazy" />
+    </div>
+    <div class="tile">
+      <nuxt-img src="/platforms/azure-functions-logo.svg" class="platform-img" loading="lazy" />
+    </div>
+    <div class="tile">
+      <nuxt-img src="/platforms/google-cloud-functions-logo.svg" class="platform-img" loading="lazy" />
+    </div>
+  </div>
+</template>
+<style scoped>
+.platform-tiles {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  justify-content: space-between;
+  align-items: center;
+}
+.tile {
+  width: 200px;
+  padding: 2rem;
+}
+
+@media screen and (max-width: 600px) {
+  .platform-tiles {
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ website/components/content/TypedTitle.vue
@@ -0,0 +1,53 @@
+<script setup>
+import VueWriter from 'vue-writer';
+const titles = ['At The Speed Of Light ⚡', 'Safe To Refactor', 'Serverless Ready ☁️', 'Developer Friendly 😃'];
+</script>
+
+<template>
+  <span class="typed-title">
+    <VueWriter :array="titles" :delay="3000" caret="underscore" />
+  </span>
+</template>
+
+<style>
+.is-typed {
+  display: inline;
+}
+.is-typed span.underscore {
+  display: inline-flex;
+  width: 2rem;
+  height: 0.4rem;
+  align-items: flex-end;
+  background-color: var(--elements-text-primary-color-static);
+  animation: blink 1.5s infinite;
+  margin-left: 0.5rem;
+}
+.is-typed span.cursor.typing {
+  animation: none;
+}
+@keyframes blink {
+  49% {
+    background-color: var(--elements-text-primary-color-static);
+  }
+  50% {
+    background-color: transparent;
+  }
+  99% {
+    background-color: transparent;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .typed-title {
+    min-height: 2.8rem;
+    display: block;
+    width: 100%;
+  }
+  .is-typed {
+    font-size: 1.5rem;
+  }
+  .is-typed span.underscore {
+    display: none;
+  }
+}
+</style>
