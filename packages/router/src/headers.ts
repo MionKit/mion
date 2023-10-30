93652289a246cfac4ef258085514aa20168fd770
Ma jerez <jerez.m82@gmail.com>
2023-10-30 22:39:32 +0000

    move creation of MionHeaders from node Request & Response into the router package

--- packages/router/src/headers.ts
+++ packages/router/src/headers.ts
@@ -5,12 +5,15 @@
  * The software is provided "as is", without warranty of any kind.
  * ######## */
 
+import {IncomingMessage, ServerResponse} from 'http';
 import type {HeaderValue, MionHeaders} from './types/context';
 import {isSingleValueHeader} from './types/guards';
 
-export function headersFromRecord(headers: Record<string, HeaderValue> = {}, toLowerCase = true): MionHeaders {
+// ############# PUBLIC METHODS #############
+
+export function headersFromRecord(headers: Record<string, HeaderValue> = {}, initToLowercase = true): MionHeaders {
     const mionHeaders = {
-        items: headerKeysToLowerCase(headers, toLowerCase),
+        items: headerKeysToLowerCase(headers, initToLowercase),
         append: (name: string, value: HeaderValue) => {
             const lowerName = name.toLowerCase();
             const currentValue = mionHeaders.items[lowerName];
@@ -37,8 +40,30 @@ export function headersFromRecord(headers: Record<string, HeaderValue> = {}, toL
     return mionHeaders;
 }
 
-function headerKeysToLowerCase(headers: Record<string, HeaderValue>, toLowerCase = true): Record<string, HeaderValue> {
-    if (!toLowerCase) return headers;
+export function headersFromIncomingMessage(rawRequest: IncomingMessage): MionHeaders {
+    // IncomingMessage headers already in lowercase or case insensitive so no need to transform
+    const initToLowercase = false;
+    return headersFromRecord(rawRequest.headers as Record<string, HeaderValue>, initToLowercase);
+}
+
+export function headersFromServerResponse(resp: ServerResponse, initialHeaders: Record<string, HeaderValue> = {}): MionHeaders {
+    Object.entries(initialHeaders).forEach(([name, value]) => resp.setHeader(name, value));
+    return {
+        append: (name: string, value: HeaderValue) => resp.appendHeader(name.toLowerCase(), value as any as string | string[]),
+        delete: (name: string) => resp.removeHeader(name.toLowerCase()),
+        get: (name: string) => resp.getHeader(name.toLowerCase()) as HeaderValue,
+        has: (name: string) => resp.hasHeader(name.toLowerCase()),
+        set: (name: string, value: HeaderValue) => resp.setHeader(name.toLowerCase(), value),
+        entries: () => new Map(Object.entries(resp.getHeaders() as Record<string, HeaderValue>)).entries(),
+        keys: () => new Set(resp.getHeaderNames()).values(),
+        values: () => new Set(Object.values(resp.getHeaders() as Record<string, HeaderValue>)).values(),
+    };
+}
+
+// ############# PRIVATE METHODS #############
+
+function headerKeysToLowerCase(headers: Record<string, HeaderValue>, initToLowerCase = true): Record<string, HeaderValue> {
+    if (!initToLowerCase) return headers;
     const entries = Object.entries(headers).map(([name, value]) => [name.toLowerCase(), value]);
     return Object.fromEntries(entries);
 }
