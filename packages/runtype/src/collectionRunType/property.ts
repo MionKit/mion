86d7c3e3855bfe454700535a24c30c671acf0ec8
Ma jerez <jerez.m82@gmail.com>
2024-03-28 19:27:58 +0000

    simplify property stringify

--- packages/runtype/src/collectionRunType/property.ts
+++ packages/runtype/src/collectionRunType/property.ts
@@ -14,7 +14,7 @@ import {BaseRunType} from '../baseRunType';
 export class PropertySignatureRunType extends BaseRunType<TypePropertySignature> {
     public readonly isJsonEncodeRequired: boolean;
     public readonly isJsonDecodeRequired: boolean;
-    public readonly memberType: RunType;
+    public readonly memberRunType: RunType;
     public readonly name: string;
     public readonly isOptional: boolean;
     public readonly isReadonly: boolean;
@@ -29,7 +29,7 @@ export class PropertySignatureRunType extends BaseRunType<TypePropertySignature>
         public readonly opts: RunTypeOptions
     ) {
         super(visitor, src, nestLevel, opts);
-        this.memberType = visitor(src.type, nestLevel, opts);
+        this.memberRunType = visitor(src.type, nestLevel, opts);
 
         this.isOptional = !!src.optional;
         this.isReadonly = !!src.readonly;
@@ -45,42 +45,42 @@ export class PropertySignatureRunType extends BaseRunType<TypePropertySignature>
         } else {
             this.shouldSerialize = !isFunctionKind(src.kind);
             // forces encode & ignore the symbol
-            this.isJsonEncodeRequired = this.memberType.isJsonEncodeRequired && this.shouldSerialize;
+            this.isJsonEncodeRequired = this.memberRunType.isJsonEncodeRequired && this.shouldSerialize;
             // either symbol is not present or should be ignored using this.opts?.strictJSON
-            this.isJsonDecodeRequired = this.memberType.isJsonDecodeRequired;
+            this.isJsonDecodeRequired = this.memberRunType.isJsonDecodeRequired;
             this.isSafePropName =
                 (typeof src.name === 'string' && validPropertyNameRegExp.test(src.name)) || typeof src.name === 'number';
             this.propName = src.name;
             this.safeAccessor = this.isSafePropName ? `.${src.name}` : `[${toLiteral(src.name)}]`;
         }
 
-        this.name = `${this.propName}${this.isOptional ? '?' : ''}:${this.memberType.name}`;
+        this.name = `${this.propName}${this.isOptional ? '?' : ''}:${this.memberRunType.name}`;
     }
     JIT_isType(varName: string): string {
         if (!this.shouldSerialize) return '';
         const accessor = `${varName}${this.safeAccessor}`;
         if (this.isOptional) {
-            return `${accessor} === undefined || (${this.memberType.JIT_isType(accessor)})`;
+            return `${accessor} === undefined || (${this.memberRunType.JIT_isType(accessor)})`;
         }
-        return this.memberType.JIT_isType(accessor);
+        return this.memberRunType.JIT_isType(accessor);
     }
     JIT_typeErrors(varName: string, errorsName: string, pathChain: string): string {
         if (!this.shouldSerialize) return '';
         const accessor = `${varName}${this.safeAccessor}`;
         if (this.isOptional) {
-            return `if (${accessor} !== undefined) {${this.memberType.JIT_typeErrors(
+            return `if (${accessor} !== undefined) {${this.memberRunType.JIT_typeErrors(
                 accessor,
                 errorsName,
                 addToPathChain(pathChain, this.propName)
             )}}`;
         }
-        return this.memberType.JIT_typeErrors(accessor, errorsName, addToPathChain(pathChain, this.propName));
+        return this.memberRunType.JIT_typeErrors(accessor, errorsName, addToPathChain(pathChain, this.propName));
     }
     JIT_jsonEncode(varName: string): string {
         if (!this.shouldSerialize) return '';
         const accessor = `${varName}${this.safeAccessor}`;
         const useNative = skipJsonEncode(this);
-        const propCode = useNative ? '' : this.memberType.JIT_jsonEncode(accessor);
+        const propCode = useNative ? '' : this.memberRunType.JIT_jsonEncode(accessor);
         if (!propCode) return '';
         if (this.isOptional) {
             return `if (${accessor} !== undefined) ${propCode}`;
@@ -91,27 +91,28 @@ export class PropertySignatureRunType extends BaseRunType<TypePropertySignature>
         if (!this.shouldSerialize) return '';
         const accessor = `${varName}${this.safeAccessor}`;
         const useNative = skipJsonDecode(this);
-        const propCode = useNative ? '' : this.memberType.JIT_jsonDecode(accessor);
+        const propCode = useNative ? '' : this.memberRunType.JIT_jsonDecode(accessor);
         if (!propCode) return '';
         if (this.isOptional) {
             return `if (${accessor} !== undefined) ${propCode}`;
         }
         return propCode;
     }
-    JIT_jsonStringify(varName: string, isFirst = false): string {
+    JIT_jsonStringify(varName: string, isFisrt = false): string {
         if (!this.shouldSerialize) return '';
         // when is not safe firs stringify sanitizes string, second output double quoted scaped json string
         const proNameJSon = this.isSafePropName ? `'${toLiteral(this.propName)}'` : asJSONString(toLiteral(this.propName));
         const accessor = `${varName}${this.safeAccessor}`;
-        const propCode = this.memberType.JIT_jsonStringify(accessor);
+        const propCode = this.memberRunType.JIT_jsonStringify(accessor);
+        const sep = isFisrt ? '' : `','+`;
         if (this.isOptional) {
-            return `${isFirst ? '' : '+'}(${accessor} === undefined ?'':${isFirst ? '' : `(','+`}${proNameJSon}+':'+${propCode}))`;
+            return `(${accessor} === undefined ? '' : ${sep}${proNameJSon}+':'+${propCode})`;
         }
-        return `${isFirst ? '' : `+','+`}${proNameJSon}+':'+${propCode}`;
+        return `${sep}${proNameJSon}+':'+${propCode}`;
     }
     mock(optionalProbability = 0.2, ...args: any[]): any {
         if (optionalProbability < 0 || optionalProbability > 1) throw new Error('optionalProbability must be between 0 and 1');
         if (this.isOptional && Math.random() < optionalProbability) return undefined;
-        return this.memberType.mock(...args);
+        return this.memberRunType.mock(...args);
     }
 }
