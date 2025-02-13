44e0b31e7dd4278e47006ae8e394fa13010a4f11
Ma jerez <jerez.m82@gmail.com>
2025-02-13 23:01:34 +0000

    rename branded types

---
+++ packages/runtype/src/brandedtypes/string.spec.ts
@@ -0,0 +1,27 @@
+/* ########
+ * 2024 mion
+ * Author: Ma-jerez
+ * License: MIT
+ * The software is provided "as is", without warranty of any kind.
+ * ######## */
+import {JitFunctions} from '../constants';
+import {runType} from '../runType';
+import {StringFormat} from './string';
+
+it.only('validate string max length', () => {
+    type Max5 = StringFormat<{maxLength: 5}>;
+    const rt = runType<Max5>();
+    const isType = rt.createJitFunction(JitFunctions.isType);
+    expect(isType('a'.repeat(4))).toBe(true);
+    expect(isType('a'.repeat(5))).toBe(true);
+    expect(isType('a'.repeat(6))).toBe(false);
+});
+
+it('validate string min length', () => {
+    type Min5 = StringFormat<{minLength: 5}>;
+    const rt = runType<Min5>();
+    const isType = rt.createJitFunction(JitFunctions.isType);
+    expect(isType('a'.repeat(4))).toBe(false);
+    expect(isType('a'.repeat(5))).toBe(true);
+    expect(isType('a'.repeat(6))).toBe(true);
+});
