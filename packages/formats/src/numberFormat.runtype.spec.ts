0483d813e9e549e3e4fe68e1e842f048debf6def
Ma jerez <jerez.m82@gmail.com>
2025-04-13 00:10:08 +0100

    number format first implementation

---
+++ packages/formats/src/numberFormat.runtype.spec.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* ########
+ * 2025 mion
+ * Author: Ma-jerez
+ * License: MIT
+ * The software is provided "as is", without warranty of any kind.
+ * ######## */
