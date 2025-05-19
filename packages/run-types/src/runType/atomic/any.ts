4c9bf802f53afcfeeeeb1c8489676aed19372995
Ma jerez <jerez.m82@gmail.com>
2025-05-19 01:42:54 +0100

    revert and generate jitFunctions for type any as is causing too much trouble otherwise, by default will use JSON stringily and fail at runType. maybe another option to add to compiler options?

--- packages/run-types/src/runType/atomic/any.ts
+++ packages/run-types/src/runType/atomic/any.ts
@@ -6,7 +6,6 @@
  * ######## */
 
 import {ReflectionKind, type TypeAny, type TypeUnknown} from '@deepkit/type';
-import type {jitCode} from '../../types';
 import {AtomicRunType} from '../../lib/baseRunTypes';
 
 export class AnyRunType extends AtomicRunType<TypeAny | TypeUnknown> {
@@ -16,39 +15,10 @@ export class AnyRunType extends AtomicRunType<TypeAny | TypeUnknown> {
         return this.src.kind === ReflectionKind.any ? 'any' : 'unknown';
     }
 
-    _compileIsType(): jitCode {
-        throw new Error(`Cannot compile isType for ${this.getTypeKindName()} type.`);
+    _compileIsType(): undefined {
+        return undefined;
     }
-
-    _compileTypeErrors(): jitCode {
-        throw new Error(`Cannot compile typeErrors for ${this.getTypeKindName()} type.`);
-    }
-
-    _compileToJsonVal(): jitCode {
-        throw new Error(`Cannot compile toJsonVal for ${this.getTypeKindName()} type.`);
-    }
-
-    _compileFromJsonVal(): jitCode {
-        throw new Error(`Cannot compile fromJsonVal for ${this.getTypeKindName()} type.`);
-    }
-
-    _compileJsonStringify(): jitCode {
-        throw new Error(`Cannot compile JsonStringify for ${this.getTypeKindName()} type.`);
-    }
-
-    _compileHasUnknownKeys(): jitCode {
-        throw new Error(`Cannot compile hasUnknownKeys for ${this.getTypeKindName()} type.`);
-    }
-
-    _compileUnknownKeyErrors(): jitCode {
-        throw new Error(`Cannot compile unknownKeyErrors for ${this.getTypeKindName()} type.`);
-    }
-
-    _compileStripUnknownKeys(): jitCode {
-        throw new Error(`Cannot compile stripUnknownKeys for ${this.getTypeKindName()} type.`);
-    }
-
-    _compileUnknownKeysToUndefined(): jitCode {
-        throw new Error(`Cannot compile unknownKeysToUndefined for ${this.getTypeKindName()} type.`);
+    _compileTypeErrors(): undefined {
+        return undefined;
     }
 }
