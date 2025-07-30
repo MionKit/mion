f7ca0b0d219bac9625016abaffc69978ebf3636b
Ma Jerez <marlon.jerez@teamwork.com>
2025-07-30 07:48:32 +0100

    add json parse todo file

---
+++ packages/run-types/src/jitFnsCompilers/jsonParse.ts
@@ -0,0 +1,16 @@
+/* ########
+ * 2025 mion
+ * Author: Ma-jerez
+ * License: MIT
+ * The software is provided "as is", without warranty of any kind.
+ * ######## */
+
+/* 
+TODO: implement jsonParse following the same logic as jsonStringify.ts
+
+we must use the same structure to traverse all existing runType nodes 
+but instead generating a valid json string we need to parse a valid json string into the correct object structure.
+for object properties we already know the name so that would give us a performance advantage.
+
+create a new jsonParse.spec.ts to test all cases check jsonStringify.spec.ts to follow the same format.
+*/
