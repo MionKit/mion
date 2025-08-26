d810f8742f4ac47304749eefba9ffed7e2a139ec
Ma jerez <jerez.m82@gmail.com>
2025-08-26 23:09:04 +0100

    fix most of the examples

--- packages/router/examples/hooks-header-definition.routes.ts
+++ packages/router/examples/hooks-header-definition.routes.ts
@@ -3,7 +3,7 @@ import {getAuthUser, isAuthorized} from 'MyAuth';
 
 const routes = {
     // using the headersHook function to define a hook
-    auth: headersHook('authorization', async (ctx, token: string): Promise<void> => {
+    auth: headersHook(['authorization'], async (ctx, token: string): Promise<void> => {
         const me = await getAuthUser(token);
         if (!isAuthorized(me)) throw {code: 401, message: 'user is not authorized'};
         ctx.shared.auth = {me}; // user is added to ctx to shared with other routes/hooks
