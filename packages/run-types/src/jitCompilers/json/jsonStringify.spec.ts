ee8f16ac1ea2d4b9def7aaaab56c6f5c2dfd8cca
Ma Jerez <M-jerez@users.noreply.github.com>
2025-09-24 20:05:20 +0000

    Move jsonStringify test from atomic/string.spec.ts
    - Created packages/run-types/src/jitCompilers/json/jsonStringify.spec.ts
    - Moved test from packages/run-types/src/runType/atomic/string.spec.ts:34-40
    - All tests passing

---
+++ packages/run-types/src/jitCompilers/json/jsonStringify.spec.ts
@@ -0,0 +1,26 @@
+/* ########
+ * 2024 mion
+ * Author: Ma-jerez
+ * License: MIT
+ * The software is provided "as is", without warranty of any kind.
+ * ######## */
+
+import {runType} from '../../lib/runType';
+import {JitFunctions} from '../../constants.functions';
+
+describe('jsonStringify compilation tests', () => {
+    // Tests moved from various spec files under packages/run-types/src/runType/
+
+    // Moved from packages/run-types/src/runType/atomic/string.spec.ts:34-40
+    {
+        const rt = runType<string>();
+
+        it('json stringify', () => {
+            const jsonStringify = rt.createJitFunction(JitFunctions.jsonStringify);
+            const fromJsonVal = rt.createJitFunction(JitFunctions.fromJsonVal);
+            const typeValue = 'hello';
+            const roundTrip = fromJsonVal(JSON.parse(jsonStringify(typeValue)));
+            expect(roundTrip).toEqual(typeValue);
+        });
+    }
+});
