4e037b2d5fc0127a8aceb25414977b28b39d2124
Ma Jerez <M-jerez@users.noreply.github.com>
2025-09-24 20:11:38 +0000

    Move jsonStringify tests from remaining atomic files
    - Moved tests from packages/run-types/src/runType/atomic/undefined.spec.ts:40-46
    - Moved tests from packages/run-types/src/runType/atomic/number.spec.ts:39-45
    - Moved tests from packages/run-types/src/runType/atomic/date.spec.ts:31-37
    - Moved tests from packages/run-types/src/runType/atomic/enum.spec.ts:51-61
    - Moved tests from packages/run-types/src/runType/atomic/symbol.spec.ts:42-48
    - Moved tests from packages/run-types/src/runType/atomic/object.spec.ts:45-51
    - Moved tests from packages/run-types/src/runType/atomic/void.spec.ts:40-43
    - All tests passing

--- packages/run-types/src/jitCompilers/json/jsonStringify.spec.ts
+++ packages/run-types/src/jitCompilers/json/jsonStringify.spec.ts
@@ -91,4 +91,101 @@ describe('jsonStringify compilation tests', () => {
             expect(roundTrip).toEqual(typeValue);
         });
     }
+
+    // Moved from packages/run-types/src/runType/atomic/undefined.spec.ts:40-46
+    {
+        const rt = runType<undefined>();
+
+        it('json stringify undefined', () => {
+            const jsonStringify = rt.createJitFunction(JitFunctions.jsonStringify);
+            const fromJsonVal = rt.createJitFunction(JitFunctions.fromJsonVal);
+            const typeValue = undefined;
+            const roundTrip = fromJsonVal(JSON.parse(jsonStringify(typeValue)));
+            expect(roundTrip).toEqual(typeValue);
+        });
+    }
+
+    // Moved from packages/run-types/src/runType/atomic/number.spec.ts:39-45
+    {
+        const rt = runType<number>();
+
+        it('json stringify number', () => {
+            const jsonStringify = rt.createJitFunction(JitFunctions.jsonStringify);
+            const fromJsonVal = rt.createJitFunction(JitFunctions.fromJsonVal);
+            const typeValue = 42;
+            const roundTrip = fromJsonVal(JSON.parse(jsonStringify(typeValue)));
+            expect(roundTrip).toEqual(typeValue);
+        });
+    }
+
+    // Moved from packages/run-types/src/runType/atomic/date.spec.ts:31-37
+    {
+        const rt = runType<Date>();
+
+        it('json stringify date', () => {
+            const jsonStringify = rt.createJitFunction(JitFunctions.jsonStringify);
+            const fromJsonVal = rt.createJitFunction(JitFunctions.fromJsonVal);
+            const typeValue = new Date();
+            const roundTrip = fromJsonVal(JSON.parse(jsonStringify(typeValue)));
+            expect(roundTrip).toEqual(typeValue);
+        });
+    }
+
+    // Moved from packages/run-types/src/runType/atomic/enum.spec.ts:51-61
+    {
+        enum Color {
+            Red = 'red',
+            Green = 'green',
+            Blue = 'blue',
+        }
+        const rt = runType<Color>();
+
+        it('json stringify enum', () => {
+            const jsonStringify = rt.createJitFunction(JitFunctions.jsonStringify);
+            const fromJsonVal = rt.createJitFunction(JitFunctions.fromJsonVal);
+            const typeValue = Color.Red;
+            const roundTrip = fromJsonVal(JSON.parse(jsonStringify(typeValue)));
+            expect(roundTrip).toEqual(typeValue);
+
+            const typeValueG = Color.Green;
+            const roundTripG = fromJsonVal(JSON.parse(jsonStringify(typeValueG)));
+            expect(roundTripG).toEqual(typeValueG);
+        });
+    }
+
+    // Moved from packages/run-types/src/runType/atomic/symbol.spec.ts:42-48
+    {
+        const rt = runType<symbol>();
+
+        it('json stringify symbol', () => {
+            const jsonStringify = rt.createJitFunction(JitFunctions.jsonStringify);
+            const fromJsonVal = rt.createJitFunction(JitFunctions.fromJsonVal);
+            const typeValue = Symbol('foo');
+            const roundTrip = fromJsonVal(JSON.parse(jsonStringify(typeValue)));
+            expect(roundTrip.toString()).toEqual(typeValue.toString());
+        });
+    }
+
+    // Moved from packages/run-types/src/runType/atomic/object.spec.ts:45-51
+    {
+        const rt = runType<object>();
+
+        it('json stringify object', () => {
+            const jsonStringify = rt.createJitFunction(JitFunctions.jsonStringify);
+            const fromJsonVal = rt.createJitFunction(JitFunctions.fromJsonVal);
+            const typeValue = {a: 42, b: 'hello'};
+            const roundTrip = fromJsonVal(JSON.parse(jsonStringify(typeValue)));
+            expect(roundTrip).toEqual(typeValue);
+        });
+    }
+
+    // Moved from packages/run-types/src/runType/atomic/void.spec.ts:40-43
+    {
+        const rt = runType<void>();
+
+        it('json stringify should return undefined', () => {
+            const stringify = rt.createJitFunction(JitFunctions.jsonStringify);
+            expect(stringify(undefined)).toBe(undefined);
+        });
+    }
 });
