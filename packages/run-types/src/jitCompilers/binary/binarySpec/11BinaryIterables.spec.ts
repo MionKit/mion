6b81f6076d65c88175b2c600d61446f034c8cbb1
Ma jerez <jerez.m82@gmail.com>
2025-10-18 17:04:52 +0100

    split binary spec

---
+++ packages/run-types/src/jitCompilers/binary/binarySpec/11BinaryIterables.spec.ts
@@ -0,0 +1,116 @@
+/* ########
+ * 2025 mion
+ * Author: Ma-jerez
+ * License: MIT
+ * The software is provided "as is", without warranty of any kind.
+ * ######## */
+
+import {SERIALIZATION_SPEC} from '../../serialization-suite';
+import {createSerializationFns, roundTrip} from './binaryHelpers';
+
+let ranTests = 0;
+afterEach(() => ranTests++);
+
+it('Set', () => {
+    const {rt, values} = SERIALIZATION_SPEC.ITERABLES.set.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.ITERABLES.set.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {deserialized} = roundTrip(serialize, deserialize, value);
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('Set<SmallObject>', () => {
+    const {rt, values} = SERIALIZATION_SPEC.ITERABLES.set_small_object.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.ITERABLES.set_small_object.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {deserialized} = roundTrip(serialize, deserialize, value);
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('objects with nested sets', () => {
+    const {rt, values} = SERIALIZATION_SPEC.ITERABLES.objects_with_nested_sets.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.ITERABLES.objects_with_nested_sets.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {deserialized} = roundTrip(serialize, deserialize, value);
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('Map', () => {
+    const {rt, values} = SERIALIZATION_SPEC.ITERABLES.map.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.ITERABLES.map.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {deserialized} = roundTrip(serialize, deserialize, value);
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('Map<string, SmallObject>', () => {
+    const {rt, values} = SERIALIZATION_SPEC.ITERABLES.map_string_small_object.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.ITERABLES.map_string_small_object.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {deserialized} = roundTrip(serialize, deserialize, value);
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('Map<SmallObject, number>', () => {
+    const {rt, values} = SERIALIZATION_SPEC.ITERABLES.map_small_object_number.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.ITERABLES.map_small_object_number.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {deserialized} = roundTrip(serialize, deserialize, value);
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('Map with bigint keys', () => {
+    const {rt, values} = SERIALIZATION_SPEC.ITERABLES.map_with_bigint_keys.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.ITERABLES.map_with_bigint_keys.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {deserialized} = roundTrip(serialize, deserialize, value);
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('Map with date values', () => {
+    const {rt, values} = SERIALIZATION_SPEC.ITERABLES.map_with_date_values.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.ITERABLES.map_with_date_values.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {deserialized} = roundTrip(serialize, deserialize, value);
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('objects with nested maps', () => {
+    const {rt, values} = SERIALIZATION_SPEC.ITERABLES.objects_with_nested_maps.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.ITERABLES.objects_with_nested_maps.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {deserialized} = roundTrip(serialize, deserialize, value);
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('all test ran', () => {
+    const totalTest = Object.keys(SERIALIZATION_SPEC.ITERABLES).length;
+    expect(ranTests).toBe(totalTest);
+});
---
+++ packages/run-types/src/jitCompilers/binary/binarySpec/binaryHelpers.ts
@@ -0,0 +1,92 @@
+/* ########
+ * 2025 mion
+ * Author: Ma-jerez
+ * License: MIT
+ * The software is provided "as is", without warranty of any kind.
+ * ######## */
+
+import {JitFunctions} from '../../../constants.functions';
+import {FunctionRunType} from '../../../runType/function/function';
+import type {DataViewDeserializer, DataViewSerializer, StrictArrayBuffer} from './../types';
+import type {InterfaceRunType} from '../../../runType/collection/interface';
+import type {RunType} from '../../../types';
+import {createDataViewDeserializer, createDataViewSerializer} from './../binarySerializerDataView';
+
+const serContext: DataViewSerializer = createDataViewSerializer({bufferSize: 1024});
+const desContext: DataViewDeserializer = createDataViewDeserializer(new ArrayBuffer(0));
+
+const SERIALIZE_FN = JitFunctions.toBinary;
+const DESERIALIZE_FN = JitFunctions.fromBinary;
+
+export function createSerializationFns(rt: RunType) {
+    const toBinary = rt.createJitFunction(SERIALIZE_FN);
+    const fromBinary = rt.createJitFunction(DESERIALIZE_FN);
+    const serialize = (v: any) => (toBinary(v, serContext), serContext.getBuffer());
+    const deserialize = (data: StrictArrayBuffer) => (desContext.setBuffer(data), fromBinary(undefined, desContext));
+    return {serialize, deserialize};
+}
+
+export function createSerializationParamsFn(rt: FunctionRunType, sliceStart?: number) {
+    const params = typeof sliceStart === 'number' ? {paramsSlice: {start: sliceStart}} : undefined;
+    const toBinary = rt.createJitParamsFunction(SERIALIZE_FN, params);
+    const fromBinary = rt.createJitParamsFunction(DESERIALIZE_FN, params);
+    const serialize = (v: any) => (toBinary(v, serContext), serContext.getBuffer());
+    const deserialize = (data: StrictArrayBuffer) => (desContext.setBuffer(data), fromBinary(undefined, desContext));
+    return {serialize, deserialize};
+}
+
+export function createSerializationReturnFn(rt: FunctionRunType) {
+    const toBinary = rt.createJitReturnFunction(SERIALIZE_FN);
+    const fromBinary = rt.createJitReturnFunction(DESERIALIZE_FN);
+    const serialize = (v: any) => (toBinary(v, serContext), serContext.getBuffer());
+    const deserialize = (data: StrictArrayBuffer) => (desContext.setBuffer(data), fromBinary(undefined, desContext));
+    return {serialize, deserialize};
+}
+
+export function createSerializationCallSignatureParamsFn(rt: InterfaceRunType) {
+    const callSignature = rt.getCallSignature()!;
+    const toBinary = callSignature.createJitParamsFunction(SERIALIZE_FN);
+    const fromBinary = callSignature.createJitParamsFunction(DESERIALIZE_FN);
+    const serialize = (v: any) => (toBinary(v, serContext), serContext.getBuffer());
+    const deserialize = (data: StrictArrayBuffer) => (desContext.setBuffer(data), fromBinary(undefined, desContext));
+    return {serialize, deserialize};
+}
+
+export function createSerializationCallSignatureReturnFn(rt: InterfaceRunType) {
+    const callSignature = rt.getCallSignature()!;
+    const toBinary = callSignature.createJitReturnFunction(SERIALIZE_FN);
+    const fromBinary = callSignature.createJitReturnFunction(DESERIALIZE_FN);
+    const serialize = (v: any) => (toBinary(v, serContext), serContext.getBuffer());
+    const deserialize = (data: StrictArrayBuffer) => (desContext.setBuffer(data), fromBinary(undefined, desContext));
+    return {serialize, deserialize};
+}
+
+const sizesEntries: any[] = [];
+const valuesEntries: any[] = [];
+
+export function roundTrip(
+    serialize: (v: any) => StrictArrayBuffer,
+    deserialize: (v: StrictArrayBuffer) => any,
+    value: any,
+    debug = false
+) {
+    serContext.reset();
+    const serialized = serialize(value);
+    // console.log('serialized', serialized);
+    const deserialized = deserialize(serialized);
+    if (debug) {
+        let json: number | string = '';
+        // eslint-disable-next-line
+        try { json = new Blob([JSON.stringify(value)]).size; } catch (e) { json = '-'; } // prettier-ignore
+        const sizes = {name: expect.getState().currentTestName, json, binary: serialized.byteLength};
+        sizesEntries.push(sizes);
+        valuesEntries.push({value, deserialized});
+    }
+    const result = {serialized, deserialized};
+    return result;
+}
+
+afterAll(() => {
+    if (sizesEntries.length) console.table(sizesEntries);
+    if (valuesEntries.length) console.table(valuesEntries);
+});
