e3e1c0b1add37bab29646f8c1a8d76310c2ee595
Ma jerez <jerez.m82@gmail.com>
2025-10-18 16:49:00 +0100

    split json spec and template in multiple files as a single file is quite slow due to ES-lint large files

---
+++ packages/run-types/src/jitCompilers/json/stringifySpec/03StringifyObjects.spec.ts
@@ -0,0 +1,220 @@
+/* ########
+ * 2025 mion
+ * Author: Ma-jerez
+ * License: MIT
+ * The software is provided "as is", without warranty of any kind.
+ * ######## */
+
+import {SERIALIZATION_SPEC} from '../../serialization-suite';
+import {roundTrip, createSerializationFns} from './stringifyHelpers';
+
+let ranTests = 0;
+afterEach(() => ranTests++);
+
+it('interface', () => {
+    const {rt, values} = SERIALIZATION_SPEC.OBJECTS.interface.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.OBJECTS.interface.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('interface with many optional props', () => {
+    const {rt, values} = SERIALIZATION_SPEC.OBJECTS.many_optional_props.getTestData();
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value) => {
+        const {deserialized} = roundTrip(serialize, deserialize, value);
+        expect(value).toEqual(deserialized);
+    });
+});
+
+it('undefined in object', () => {
+    const {rt, values} = SERIALIZATION_SPEC.OBJECTS.undefined_in_object.getTestData();
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(value).toEqual(deserialized);
+    });
+});
+
+it('class', () => {
+    const {rt, values, deserializedValues} = SERIALIZATION_SPEC.OBJECTS.class.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.OBJECTS.class.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(deserialized).toEqual(deserializedValues[i]);
+        // deserialized object is a plain object and not a class instance
+        expect(deserialized.constructor.name).not.toEqual(originalValues[i].constructor.name);
+    });
+});
+
+it('class can be deserialized after registered', async () => {
+    const {rt, values} = SERIALIZATION_SPEC.OBJECTS.serializable_class_restored.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.OBJECTS.serializable_class_restored.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(originalValues[i]).toEqual(deserialized);
+        // class has been registered for deserialization so we get a class instance back
+        expect(deserialized.constructor.name).toEqual(originalValues[i].constructor.name);
+    });
+});
+
+it('deserialize class using a function', () => {
+    const {rt, values} = SERIALIZATION_SPEC.OBJECTS.classes_deserialize_function.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.OBJECTS.classes_deserialize_function.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('extended class', () => {
+    const {rt, values} = SERIALIZATION_SPEC.OBJECTS.extended_class.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.OBJECTS.extended_class.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('RpcError class are restored to class by default', () => {
+    const {rt, values} = SERIALIZATION_SPEC.OBJECTS.rpc_error_class.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.OBJECTS.rpc_error_class.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(originalValues[i]).toEqual(deserialized);
+        expect(deserialized.constructor.name).toEqual(originalValues[i].constructor.name);
+    });
+});
+
+it('optional properties', () => {
+    const {rt, values} = SERIALIZATION_SPEC.OBJECTS.optional_properties_order.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.OBJECTS.optional_properties_order.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('should work when all fields are optional', () => {
+    const {rt, values} = SERIALIZATION_SPEC.OBJECTS.all_optional_fields.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.OBJECTS.all_optional_fields.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('strip extra params', () => {
+    const {rt, values, deserializedValues} = SERIALIZATION_SPEC.OBJECTS.strip_extra_params.getTestData();
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(deserializedValues[i]).toEqual(deserialized);
+    });
+});
+
+it('interface circular', () => {
+    const {rt, values} = SERIALIZATION_SPEC.OBJECTS.interface_circular.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.OBJECTS.interface_circular.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('interface circular array', () => {
+    const {rt, values} = SERIALIZATION_SPEC.OBJECTS.interface_circular_array.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.OBJECTS.interface_circular_array.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('interface circular deep', () => {
+    const {rt, values} = SERIALIZATION_SPEC.OBJECTS.interface_circular_deep.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.OBJECTS.interface_circular_deep.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('interface root not circular', () => {
+    const {rt, values} = SERIALIZATION_SPEC.OBJECTS.interface_root_not_circular.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.OBJECTS.interface_root_not_circular.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('interface multiple circular', () => {
+    const {rt, values} = SERIALIZATION_SPEC.OBJECTS.interface_multiple_circular.getTestData();
+    const {values: originalValues} = SERIALIZATION_SPEC.OBJECTS.interface_multiple_circular.getTestData(true);
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {serialized, deserialized} = roundTrip(serialize, deserialize, value);
+        expect(typeof serialized).toBe('string');
+        expect(originalValues[i]).toEqual(deserialized);
+    });
+});
+
+it('methods should be excluded from interface', () => {
+    const {rt, values, deserializedValues} = SERIALIZATION_SPEC.OBJECTS.interface_with_methods.getTestData();
+    const {serialize, deserialize} = createSerializationFns(rt);
+
+    values.forEach((value, i) => {
+        const {deserialized} = roundTrip(serialize, deserialize, value);
+        expect(deserialized).toEqual(deserializedValues[i]);
+    });
+});
+
+it('all test ran', () => {
+    const totalTest = Object.keys(SERIALIZATION_SPEC.OBJECTS).length;
+    expect(ranTests).toBe(totalTest);
+});
