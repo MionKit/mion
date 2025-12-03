21523d6dda933b74961139f2a557046a75e12c43
Ma jerez <jerez.m82@gmail.com>
2025-12-03 03:42:29 +0000

    add AOT package for client routes.

---
+++ packages/client/aot/src/pureFns.cache.ts
@@ -0,0 +1,8 @@
+// ###### DO NOT MODIFY MANUALLY: THIS FILE IS GENERATED AUTOMATICALLY
+import type {PersistedPureFunctionsCache} from '@mionkit/core';
+
+/**
+ * Pure functions cache placeholder - this will be replaced with actual compiled pure functions
+ * when the AOT compilation process runs.
+ */
+export const pureFnsCache: PersistedPureFunctionsCache = {};
