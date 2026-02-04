7eba0b1ac2169486c1859bf78faaea6fe953c77a
Ma jerez <jerez.m82@gmail.com>
2026-02-04 18:49:36 +0000

    rename http package to node

--- packages/examples/src/http/node-config.ts
+++ packages/examples/src/http/node-config.ts
@@ -1,8 +1,7 @@
-import {initMionHttp} from '@mionkit/http';
+import {initMionHttp} from '@mionkit/node';
 import {routes} from './routes';
 
 const handler = initMionHttp(routes, {
     prefix: '/api', // API prefix
     // ... other router options
 });
-
