d8b699e4b0a5360e6bcd1159118fcf1951c78b5e
Ma jerez <jerez.m82@gmail.com>
2026-02-13 00:11:51 +0000

    build codegen mion-aot-template

--- packages/codegen/mion-aot-template/build/cjs/src/index.d.ts
+++ packages/codegen/mion-aot-template/build/cjs/src/index.d.ts
@@ -1,4 +1,4 @@
-import { routerCache } from './router.cache';
-import { jitFnsCache } from './jitFns.cache';
-import { pureFnsCache } from './pureFns.cache';
+import { routerCache } from './router.cache.js';
+import { jitFnsCache } from './jitFns.cache.js';
+import { pureFnsCache } from './pureFns.cache.js';
 export { routerCache, jitFnsCache, pureFnsCache };
