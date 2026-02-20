c587d48f92db7b3a8ef58c9063e0ca8e9b2b9234
Ma jerez <jerez.m82@gmail.com>
2026-02-20 20:04:42 +0000

    fix couple of error in run-types test and update plan for aot virtual modules

---
+++ packages/router/src/lib/test/aotEmitter-test-router.ts
@@ -0,0 +1,40 @@
+/* ########
+ * 2025 mion
+ * Author: Ma-jerez
+ * License: MIT
+ * The software is provided "as is", without warranty of any kind.
+ * ######## */
+
+/**
+ * Test router script for AOT emitter tests.
+ * This script is spawned as a child process with MION_COMPILE=true
+ * to test that emitAOTCaches() correctly sends IPC messages.
+ *
+ * Uses initMionRouter which automatically calls emitAOTCaches() when MION_COMPILE=true.
+ */
+
+import {initMionRouter, route, linkedFn, Routes, PublicApi} from '@mionkit/router';
+
+interface User {
+    id: string;
+    name: string;
+}
+
+const routes = {
+    users: {
+        getUser: route((ctx, id: string): User => ({id, name: 'Test User'})),
+        createUser: route((ctx, user: User): string => `Created user ${user.name}`),
+    },
+    utils: {
+        sum: route((ctx, a: number, b: number): number => a + b),
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    log: linkedFn((ctx): void => {
+        // Log linkedFn
+    }),
+} satisfies Routes;
+
+// Initialize the router - this will call emitAOTCaches() automatically when MION_COMPILE=true
+export const testApiPromise: Promise<PublicApi<typeof routes>> = initMionRouter(routes, {prefix: 'api/v1'});
+
+export {routes};
