8681958edfed4982f479cbabb24497917fd6ec2e
Ma jerez <jerez.m82@gmail.com>
2026-02-26 23:28:13 +0000

    fix AOT cache loading in the router

---
+++ packages/router/src/lib/aotCacheLoader.ts
@@ -0,0 +1,15 @@
+/* ########
+ * 2026 mion
+ * Author: Ma-jerez
+ * License: MIT
+ * The software is provided "as is", without warranty of any kind.
+ * ######## */
+
+import {jitFnsCache, pureFnsCache, routerCache} from 'virtual:mion-aot/caches';
+import {addAOTCaches, addRoutesToCache} from '@mionkit/core';
+
+/** Loads pre-compiled AOT caches from virtual modules and registers them in the global caches. */
+export function loadAOTCaches(): void {
+    addAOTCaches(jitFnsCache, pureFnsCache);
+    addRoutesToCache(routerCache);
+}
