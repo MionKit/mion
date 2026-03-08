f8614adfd4ff0050db50781ed80e75479cec94e6
Ma jerez <jerez.m82@gmail.com>
2026-03-08 22:56:40 +0000

    update home page to use query instead route

--- packages/examples/src/_homepage/home-server.ts
+++ packages/examples/src/_homepage/home-server.ts
@@ -1,4 +1,4 @@
-import {initMionRouter, route, Routes} from '@mionjs/router';
+import {initMionRouter, query, route, Routes} from '@mionjs/router';
 import {startNodeServer} from '@mionjs/platform-node';
 // @annotate: Automatic Validation and Serialization from Typescript types
 
@@ -17,13 +17,13 @@ interface Order {
 // @annotate: Object based router with rpc methods that receive Fully Validated params
 
 const routes = {
-    getUser: route((ctx, id: number): User | null => {
+    getUser: query((ctx, id: number): User | null => {
         if (id !== 1234) return null;
         const tags =  new Set(['tag1', 'tag2'])
         const user: User = {id: 1234, name: 'John',age: 30, createdAt: new Date(), tags};
         return user;
     }),
-    getOrder: route((ctx, id: string): Order | null => {
+    getOrder: query((ctx, id: string): Order | null => {
         if (id !== 'ORDER-123') return null;
         const order: Order = {id: 'ORDER-123', userId: 1234, amount: 100};
         return order;
